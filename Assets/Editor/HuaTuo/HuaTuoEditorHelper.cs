ffdf8036940e11fad065f1528ff72c6de4bdd218
yangjian <eelgame@qq.com>
2022-05-10 11:20:49 +0800

    Initial commit

---
+++ Assets/Editor/HuaTuo/HuaTuoEditorHelper.cs
@@ -0,0 +1,85 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+namespace HuaTuo
+{
+    /// <summary>
+    ///     这里仅仅是一个流程展示
+    ///     简单说明如果你想将huatuo的dll做成自动化的简单实现
+    /// </summary>
+    public class HuaTuoEditorHelper
+    {
+        public static string DllBuildOutputDir => Path.GetFullPath($"{Application.dataPath}/../Temp/HuaTuo/build");
+
+
+        public static string AssetBundleOutputDir => Application.dataPath + "/HuaTuo/Output";
+
+        private static void CreateDirIfNotExists(string dirName)
+        {
+            if (!Directory.Exists(dirName)) Directory.CreateDirectory(dirName);
+        }
+
+        private static void CompileDll(string buildDir, BuildTarget target)
+        {
+            var group = BuildPipeline.GetBuildTargetGroup(target);
+
+            var scriptCompilationSettings = new ScriptCompilationSettings();
+            scriptCompilationSettings.group = group;
+            scriptCompilationSettings.target = target;
+            CreateDirIfNotExists(buildDir);
+            var scriptCompilationResult =
+                PlayerBuildInterface.CompilePlayerScripts(scriptCompilationSettings, buildDir);
+            foreach (var ass in scriptCompilationResult.assemblies) Debug.LogFormat("compile assemblies:{0}", ass);
+        }
+
+        public static string GetDllBuildOutputDirByTarget(BuildTarget target)
+        {
+            return $"{DllBuildOutputDir}/{target}";
+        }
+
+        [MenuItem("HuaTuo/CompileDll/ActiveBuildTarget")]
+        public static void CompileDllActiveBuildTarget()
+        {
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+
+        [MenuItem("HuaTuo/CompileDll/Win64")]
+        public static void CompileDllWin64()
+        {
+            var target = BuildTarget.StandaloneWindows64;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+
+        [MenuItem("HuaTuo/CompileDll/Linux64")]
+        public static void CompileDllLinux()
+        {
+            var target = BuildTarget.StandaloneLinux64;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+
+        [MenuItem("HuaTuo/CompileDll/OSX")]
+        public static void CompileDllOSX()
+        {
+            var target = BuildTarget.StandaloneOSX;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+
+        [MenuItem("HuaTuo/CompileDll/Android")]
+        public static void CompileDllAndroid()
+        {
+            var target = BuildTarget.Android;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+
+        [MenuItem("HuaTuo/CompileDll/IOS")]
+        public static void CompileDllIOS()
+        {
+            //var target = EditorUserBuildSettings.activeBuildTarget;
+            var target = BuildTarget.iOS;
+            CompileDll(GetDllBuildOutputDirByTarget(target), target);
+        }
+    }
+}
---
+++ Assets/Editor/HuaTuo/HuaTuo_BuildProcessor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace HuaTuo
+{
+    public class HuaTuo_BuildProcessor : IPreprocessBuildWithReport
+    {
+        public int callbackOrder { get; }
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            
+        }
+    }
+}
